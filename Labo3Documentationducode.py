5155bd8dce01cada469415add39d5bd4587bb4a3
Nathaniel Samson <202184320@cegepsherbrooke.qc.ca>
2025-02-05 15:12:30 -0500

    Allo
    Allo

---
+++ Labo3Documentationducode.py
@@ -0,0 +1,39 @@
+m = "Mage"           #Les lignes 1 à 11 sont mes variables pour mes armes, mes personnages et mes niveaux.
+p = "Paladin"
+a = "Assassin"
+bfe = "Bâton de feu"
+bf = "Bâton de foudre"
+e = "Épee longue"
+l = "Lance"
+d = "Dagues"
+p = "Poison"
+d = "Donjon"
+f = "Forêt"
+Personnage = [m, p, a]
+Arme = [bfe, bf, e, l, d, p]
+Personnage = input(f"Choisissez votre personnage {Personnage[0]} (m), Paladin (p), Assasin (a)\n: ")
+
+if Personnage == "m":
+    weapon_choice = input("Choissisez votre arme : Bâton de feu (bfe)  ou Bâton de foudre (bf)\n: ")
+    Personage_choisi = m   #Les lignes 16 à 37 sont pour que chaque personnage est ses deux seuls fois d'armes et pas d'autres choix.
+    if weapon_choice == "bfe":
+        arme_choisi = bfe
+    elif weapon_choice == "bf":
+        arme_choisi = bf
+
+if Personnage == "p":
+    weapon_choice = input("Choissisez votre arme : Épee longue (e) ou Lance (l)\n: ")
+    Personage_choisi = p
+    if weapon_choice == "e":
+        arme_choisi = e
+    elif weapon_choice == "l":
+        arme_choisi = l
+if Personnage == "a":
+    weapon_choice = input("Choissisez votre arme : Dagues (d) ou Poison (p)\n: ")
+    Personage_choisi = a
+    if weapon_choice == "d":
+        arme_choisi = d
+    elif weapon_choice == "p":
+        arme_choisi = p
+
+Niveau = input("Choisissez votre niveau entre Donjon (d) et Forêt (f)\n: ") # Cette ligne sert seulement à donner le choix entre les deux niveaux.
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Laboratoire3
+Bienvenue dans le README
+Le but du programme Labo3 est de créer un menu de création de personnage.
+Dans démarrer le programme, il faut l'ouvrir dans Visual Studio Code et Python doit avoir été préinstaller.
+La console va être utiliser pour avoir accèes au menu.
